6c7a685e1dead4f0ce17f43c22f88d857ae5841b
andremulin <aesmulin@gmail.com>
2021-09-09 22:13:55 -0300

    add simple strategy

---
+++ simple/main.tf
@@ -0,0 +1,16 @@
+resource "aws_route53_zone" "dev_zone" {
+  name = "inc.dev"
+
+  vpc {
+    vpc_id = var.vpc_id
+  }
+}
+
+resource "aws_vpc_dhcp_options" "this" {
+  domain_name_servers = "AmazonProvidedDNS"
+}
+
+resource "aws_vpc_dhcp_options_association" "this" {
+  vpc_id          = var.vpc_id
+  dhcp_options_id = aws_vpc_dhcp_options.this.id
+}
